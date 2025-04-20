ee3d659c91611c21a493809a2b03c83211a37750
Mikhail Istomin <redeemer@inbox.ru>
2025-04-20 09:54:46 -0300

    add static folder

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
 import Fastify from 'fastify';
+import fastifyStatic from '@fastify/static';
+import path from 'path';
+import { fileURLToPath } from 'url'; // Import this to handle __dirname equivalent
 import { OpenAiService } from './services/openAiService.js';
 
 const PORT = 3000;
 
+// Create __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const fastify = Fastify({ logger: true });
+
+// Serve static files from the ./public folder
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, 'public'),
+  //prefix: '/public/', // Optional: adds a prefix to access static files
+});
 
 fastify.get('/api/card/getArticle', async (request, reply) => {
   const word = request.query.word;
-  if(!word) {
+  if (!word) {
     reply.status(400).send('Missing word parameter');
     return;
   }
   const openAiService = new OpenAiService();
   const article = await openAiService.getArticle(word);
-  return { article }
+  return { article };
 });
 
 fastify.get('/api/card/getTranslationSuggestions', async (request, reply) => {
   const word = request.query.word;
-  if(!word) {
+  if (!word) {
     reply.status(400).send('Missing word parameter');
     return;
   }
   const openAiService = new OpenAiService();
   const suggestions = await openAiService.getTranslationSuggestions(word);
-  return { suggestions }
+  return suggestions;
 });
 
 fastify.setNotFoundHandler((request, reply) => {
@@ -42,8 +55,8 @@ const start = async () => {
 };
 
 fastify.setErrorHandler((error, request, reply) => {
-  fastify.log.error(error)
-  reply.status(500).send({ error: 'Internal Server Error' })
-})
+  fastify.log.error(error);
+  reply.status(500).send({ error: 'Internal Server Error' });
+});
 
 start();
