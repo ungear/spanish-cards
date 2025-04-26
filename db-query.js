89e06271c8ca8ec511e06187cdba2a7092830d7a
Mikhail Istomin <redeemer@inbox.ru>
2025-04-26 20:25:47 -0300

    feat: add query runner

---
+++ db-query.js
@@ -0,0 +1,20 @@
+import Database from 'better-sqlite3';
+const db = new Database('./data/cards.db');
+
+// Get the query from command line arguments
+const query = process.argv[2];
+
+if (!query) {
+    console.error('Please provide a SQL query as an argument');
+    process.exit(1);
+}
+
+try {
+    const stmt = db.prepare(query);
+    const results = stmt.all();
+    console.log(JSON.stringify(results, null, 2));
+} catch (error) {
+    console.error('Error executing query:', error.message);
+} finally {
+    db.close();
+} 
---
+++ readmo.md
@@ -0,0 +1,5 @@
+## Start server
+`node server.js`
+
+## Run DB query
+`node db-query.js "select * from cards"`
